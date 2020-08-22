3d73d73203105a95d2d40040a7910682730aaa53
Gábor Csárdi <csardi.gabor@gmail.com>
2020-08-22 10:44:59 +0100

    Helpers to parse username in an URL

--- R/git-auth.R
+++ R/git-auth.R
@@ -150,12 +150,66 @@ set_env <- function(envs) {
   invisible(current)
 }
 
+get_url_username <- function(url) {
+  nm <- parse_url(url)$username
+  if (nm == "") NULL else nm
+}
+
+parse_url <- function(url) {
+  re_url <- paste0(
+    "^(?<protocol>https?)://",
+    "(?:(?<username>[^@/:]+)(?::(?<password>[^@/]+))?@)?",
+    "(?<host>[^/]+)",
+    "(?<path>.*)$"            # don't worry about query params here...
+  )
+
+  mch <- re_match(url, re_url)
+  mch[, setdiff(colnames(mch), c(".match", ".text")), drop = FALSE]
+}
+
 is_string <- function(x) {
   is.character(x) && length(x) == 1 && !is.na(x)
 }
 
 has_no_newline <- function(url) {
   ! grepl("\n", url, fixed = TRUE)
+}
+
+re_match <- function(text, pattern, perl = TRUE, ...) {
+
+  stopifnot(is.character(pattern), length(pattern) == 1, !is.na(pattern))
+  text <- as.character(text)
+
+  match <- regexpr(pattern, text, perl = perl, ...)
+
+  start  <- as.vector(match)
+  length <- attr(match, "match.length")
+  end    <- start + length - 1L
+
+  matchstr <- substring(text, start, end)
+  matchstr[ start == -1 ] <- NA_character_
+
+  res <- data.frame(
+    stringsAsFactors = FALSE,
+    .text = text,
+    .match = matchstr
+  )
+
+  if (!is.null(attr(match, "capture.start"))) {
+
+    gstart  <- attr(match, "capture.start")
+    glength <- attr(match, "capture.length")
+    gend    <- gstart + glength - 1L
+
+    groupstr <- substring(text, gstart, gend)
+    groupstr[ gstart == -1 ] <- NA_character_
+    dim(groupstr) <- dim(gstart)
+
+    res <- cbind(groupstr, res, stringsAsFactors = FALSE)
+  }
+
+  names(res) <- c(attr(match, "capture.names"), ".text", ".match")
+  res
 }
 
 null_file <- function() {

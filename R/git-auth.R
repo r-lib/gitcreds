2b27eaf387a65086f9b82cfcd62bc01272923200
Gábor Csárdi <csardi.gabor@gmail.com>
2020-08-22 10:42:53 +0100

    Refactoring, simplifying

--- R/git-auth.R
+++ R/git-auth.R
@@ -1,22 +1,14 @@
 
-# TODO: if the user has GIT_CONFIG set, then this probably breaks,
-#   because we make a copy of the GIT_CONFIG file, and amend it,
-#   but git will still use the original GIT_CONFIG. It is not clear how
-#   to fix this, maybe we should unset GIT_CONFIG temporarily, and
-#   document that it is ignored by us.
+# ------------------------------------------------------------------------
+# Public API
+# ------------------------------------------------------------------------
 
 #' @export
 
 gitcreds_get <- function(url = "https://github.com") {
 
+  stopifnot(is_string(url), has_no_newline(url))
   if (!check_for_git()) stop("You need to install git")
-
-  env <- gitcreds_env()
-  oenv <- set_env(env)
-  on.exit(set_env(oenv), add = TRUE)
-
-  ## Now we are ready to query the credential
-  input <- gitcreds_input(url)
 
   helper <- paste0(
     "credential.helper=\"! echo protocol=dummy;",
@@ -25,11 +17,57 @@ gitcreds_get <- function(url = "https://github.com") {
     "echo password=dummy\""
   )
 
+  out <- gitcreds_fill(list(url = url), c("-c", helper))
+
+  parse_gitcreds_output(out)
+}
+
+#' @export
+
+gitcreds_list_helpers <- function() {
+  if (!check_for_git()) stop("You need to install git")
+  out <- system2("git", c("config", "--get-all", "credential.helper"),
+                 stdout = TRUE, stderr = null_file())
+  clear <- rev(which(out == ""))
+  if (length(clear)) out <- out[-(1:clear[1])]
+  out
+}
+
+# ------------------------------------------------------------------------
+# Raw git credential API
+# ------------------------------------------------------------------------
+
+gitcreds_fill <- function(input, args = character()) {
+  gitcreds_run("fill", input, args)
+}
+
+gitcreds_approve <- function(creds, args = character()) {
+  gitcreds_run("approve", creds, args)
+}
+
+gitcreds_reject <- function(creds, args = character()) {
+  gitcreds_run("reject", creds, args)
+}
+
+gitcreds_run <- function(command, input, args = character()) {
+  env <- gitcreds_env()
+  oenv <- set_env(env)
+  on.exit(set_env(oenv), add = TRUE)
+
+  stdin <- create_gitcreds_input(input)
+
+  git_run(c(args, "credential", command), input = stdin)
+}
+
+# ------------------------------------------------------------------------
+# Helpers specific to git
+# ------------------------------------------------------------------------
+
+git_run <- function(args, input = NULL) {
   ## TODO: should this just fail?
-  tryCatch(
-    out <- suppressWarnings(system2(
-      "git", c("-c", helper, "credential", "fill"),
-      input = input, stdout = TRUE, stderr = null_file()
+  out <- tryCatch(
+    suppressWarnings(system2(
+      "git", args, input = input, stdout = TRUE, stderr = null_file()
     )),
     error = function(e) NULL
   )
@@ -39,11 +77,10 @@ gitcreds_get <- function(url = "https://github.com") {
     out <- NULL
   }
 
-  parse_credentials(out)
+  out
 }
 
-gitcreds_input <- function(url, ...) {
-  args <- list(url = url, ...)
+create_gitcreds_input <- function(args) {
   paste0(
     paste0(names(args), "=", args, collapse = "\n"),
     "\n\n"
@@ -51,21 +88,12 @@ gitcreds_input <- function(url, ...) {
 }
 
 gitcreds_env <- function() {
-  ## Avoid interactivity with some common credential helpers
+  # Avoid interactivity and validation with some common credential helpers
   c(
     GCM_INTERACTIVE = "Never",
     GCM_MODAL_PROMPT = "false",
     GCM_VALIDATE = "false"
   )
-}
-
-#' @export
-
-gitcreds_list_helpers <- function() {
-  if (!check_for_git()) stop("You need to install git")
-  out <- system2("git", c("config", "--get-all", "credential.helper"),
-                 stdout = TRUE, stderr = null_file())
-  out
 }
 
 check_for_git <- function() {
@@ -79,17 +107,36 @@ check_for_git <- function() {
   }, error = function(e) FALSE)
 }
 
-null_file <- function() {
-  if (.Platform$OS.type == "windows") "nul:" else "/dev/null"
-}
-
-parse_credentials <- function(txt) {
+parse_gitcreds_output <- function(txt) {
   if (is.null(txt)) return(NULL)
   if (txt[1] == "protocol=dummy") return(NULL)
   nms <- sub("=.*$", "", txt)
   vls <- sub("^[^=]+=", "", txt)
   structure(as.list(vls), names = nms)
 }
+
+gitcreds_username <- function(url = NULL) {
+  if (!check_for_git()) stop("You need to install git")
+  gitcreds_username_for_url(url) %||% gitcreds_username_generic()
+}
+
+gitcreds_username_for_url <- function(url) {
+   git_run(c(
+    "config", "--get-urlmatch", "credential.username", shQuote(url)
+  ))
+}
+
+gitcreds_username_generic <- function() {
+  git_run(c("config", "credential.username"))
+}
+
+default_username <- function() {
+  if (.Platform$OS.type == "windows") "PersonalAccessToken" else "token"
+}
+
+# ------------------------------------------------------------------------
+# Genetic helpers
+# ------------------------------------------------------------------------
 
 set_env <- function(envs) {
   current <- Sys.getenv(names(envs), NA_character_)
@@ -103,36 +150,16 @@ set_env <- function(envs) {
   invisible(current)
 }
 
-gitcreds_username <- function(url = NULL) {
-  if (!check_for_git()) stop("You need to install git")
-
-  out <- NULL
-
-  # If there is an url, then first try URL specific matching
-  if (!is.null(url)) {
-    out <- suppressWarnings(system2(
-      "git",
-      c("config", "--get-urlmatch", "credential.username", shQuote(url)),
-      stdout = TRUE, stderr = null_file()
-    ))
-
-    ## TODO should this just fail?
-    if (!is.null(attr(out, "status")) && attr(out, "status") != 0) {
-      out <- NULL
-    }
-  }
-
-  if (is.null(out)) {
-    out <- suppressWarnings(system2(
-      "git",
-      c("config", "credential.username"),
-      stdout = TRUE, stderr = null_file()
-    ))
-    ## TODO should this just fail?
-    if (!is.null(attr(out, "status")) && attr(out, "status") != 0) {
-      out <- NULL
-    }
-  }
-
-  out
+is_string <- function(x) {
+  is.character(x) && length(x) == 1 && !is.na(x)
 }
+
+has_no_newline <- function(url) {
+  ! grepl("\n", url, fixed = TRUE)
+}
+
+null_file <- function() {
+  if (.Platform$OS.type == "windows") "nul:" else "/dev/null"
+}
+
+`%||%` <- function(l, r) if (is.null(l)) r else l

be370df5b473e75d9adc5dc5b1eb8ee67e6d116d
Gábor Csárdi <csardi.gabor@gmail.com>
2020-08-23 10:59:28 +0100

    Notes for internal functions

--- R/git-auth.R
+++ R/git-auth.R
@@ -23,7 +23,7 @@
 #' `gitcreds_list_helpers()` lists the active credential helpers.
 #'
 #' # Advanced topics
-#' 
+#'
 #' ## The current working directory
 #'
 #' TODO
@@ -45,15 +45,15 @@
 #' ### The default macOS helper (`osxkeychain`)
 #'
 #' TODO
-#' 
+#'
 #' ### Git Credential Manager for Windows (`manager`)
 #'
 #' TODO
-#' 
+#'
 #' ### Git Credential Manager Core (`manager-core`)
 #'
 #' TODO
-#' 
+#'
 #' @param url URL to get or set credentials for. It may contain a user
 #' name, which is typically (but not always) used by the credential
 #' helpers. It may also contain a path, which is typically (but not always)
@@ -113,17 +113,29 @@ gitcreds_set <- function(url = "https://github.com") {
   }
 }
 
+#' Replace credentials with new ones
+#'
+#' It only works interactively, because of `menu()` in `ack_replace()` and
+#' `readline()`.
+#'
+#' We need to set a username, it is compulsory for git credential.
+#' 1. If there was one in the url, then we use that.
+#' 2. Otherwise if git has a username configured for this URL, we use that.
+#' 3. Otherwise we use the username in the credentials we are replacing.
+#'
+#' @param url URL.
+#' @param current Must not be `NULL`, and it must contain a
+#' `gitcreds` object. (Well, a named list, really.)
+#' @return Nothing.
+#'
+#' @noRd
+
 gitcreds_set_replace <- function(url, current) {
   if (!ack_replace(url, current)) {
     throw(new_error("gitcreds_abort_replace_error"))
   }
 
   pat <- readline("\n? Enter new password or token: ")
-
-  # We need to set a username, it is compulsory for git credential.
-  # 1. If there was one in the url, then we use that.
-  # 2. Otherwise if git has a username configured for this URL, we use that.
-  # 3. Otherwise we use the username in the credentials we are replacing.
 
   username <- get_url_username(url) %||%
     gitcreds_username(url) %||%
@@ -140,13 +152,23 @@ gitcreds_set_replace <- function(url, current) {
   invisible()
 }
 
+#' Set new credentials
+#'
+#' This should not replace or remove any old credentials, but of course
+#' we cannot be sure, because credential helpers pretty much do what they
+#' want.
+#'
+#' We need to set a username, it is compulsory for git credential.
+#' 1. If there was one in the url, then we use that.
+#' 2. Otherwise if git has a username configured for this URL, we use that.
+#' 3. Otherwise we use a default username.
+#'
+#' @param url URL.
+#' @return Nothing.
+#' @noRd
+
 gitcreds_set_new <- function(url) {
   pat <- readline("\n? Enter new password or token: ")
-
-  # We need to set a username, it is compulsory for git credential.
-  # 1. If there was one in the url, then we use that.
-  # 2. Otherwise if git has a username configured for this URL, we use that.
-  # 3. Otherwise we use a default username.
 
   username <- get_url_username(url) %||%
     gitcreds_username(url) %||%
@@ -198,17 +220,62 @@ format.gitcreds <- function(x, header = TRUE, ...) {
 # Raw git credential API
 # ------------------------------------------------------------------------
 
+#' Run `git credential fill`
+#'
+#' @param input Named list to pass to the credential helper.
+#' @param args Extra args, used _before_ `fill`, to allow
+#' `git -c ... fill`.
+#' @return Standard output, line by line.
+#'
+#' @seealso [gitcreds_run()] and [git_run()].
+#' @noRd
+
 gitcreds_fill <- function(input, args = character()) {
   gitcreds_run("fill", input, args)
 }
+
+#' Run `git credential approve` to add new credentials
+#'
+#' @inheritParams gitcreds_fill
+#' @param creds `gitcreds` object (named list).
+#' @return Standard output, line by line.
+#'
+#' @seealso [gitcreds_run()] and [git_run()].
+#' @noRd
 
 gitcreds_approve <- function(creds, args = character()) {
   gitcreds_run("approve", creds, args)
 }
 
+#' Run `git credential reject` to remove credentials
+#'
+#' @inheritParams gitcreds_fill
+#' @param creds `gitcreds` object (named list).
+#' @return Standard output, line by line.
+#'
+#' @seealso [gitcreds_run()] and [git_run()].
+#' @noRd
+
 gitcreds_reject <- function(creds, args = character()) {
   gitcreds_run("reject", creds, args)
 }
+
+#' Run a `git credential` command
+#'
+#' @details
+#' We set the [gitcreds_env()] environment variables, to avoid dialog boxes
+#' from some credential helpers and also validation that potentiall needs
+#' an internet connection.
+#'
+#' @param command Command name, e.g. `"fill"`.
+#' @param input Named list of input, see
+#' https://git-scm.com/docs/git-credential#IOFMT
+#' @param args Extra command line arguments, added after `git` and
+#' _before_ `command`, to allow `git -c ... fill`.
+#' @return Standard output, line by line.
+#'
+#' @seealso [git_run()].
+#' @noRd
 
 gitcreds_run <- function(command, input, args = character()) {
   env <- gitcreds_env()
@@ -224,6 +291,28 @@ gitcreds_run <- function(command, input, args = character()) {
 # Helpers specific to git
 # ------------------------------------------------------------------------
 
+#' Run a git command
+#'
+#' @details
+#' Currently we don't set the credential specific environment variables
+#' here, and credential helpers invoked by `git` behave the same way as
+#' they would from the command line.
+#'
+#' ## Errors
+#'
+#' On error `git_run()` returns an error with class `git_error` and
+#' also `gitcreds_error`. The error object includes
+#' * `args` the command line arguments,
+#' * `status`: the exit status of the command,
+#' * `stdout`: the standard output of the command, line by line.
+#' * `stderr`: the standard error of the command, line by line.
+#'
+#' @param args Command line arguments.
+#' @param input The standard input (the `input` argument of [system2()].
+#' @return Standard output, line by line.
+#'
+#' @noRd
+
 git_run <- function(args, input = NULL) {
   stderr_file <- tempfile("gitcreds-stderr-")
   on.exit(unlink(stderr_file, recursive = TRUE), add = TRUE)
@@ -238,6 +327,7 @@ git_run <- function(args, input = NULL) {
     throw(new_error(
       "git_error",
       args = args,
+      stdout = out,
       status = attr(out, "status"),
       stderr = read_file(stderr_file)
     ))
@@ -245,6 +335,18 @@ git_run <- function(args, input = NULL) {
 
   out
 }
+
+#' Request confirmation from the user, to replace credentials
+#'
+#' This function only works in interactive sessions.
+#'
+#' @param url URL to set new credentials for.
+#' @param current The current credentials.
+#' @return `FALSE` is the user changed their mind, to keep the current
+#' credentials. `TRUE` for replacing them.
+#'
+#' @seealso [gitcreds_set()].
+#' @noRd
 
 ack_replace <- function(url, current) {
   msg("\n-> Your current credentials for ", squote(url), ":\n")
@@ -266,9 +368,27 @@ ack_replace <- function(url, current) {
   }
 }
 
+#' Whether a `gitcreds` credential has a non-empty `password`
+#'
+#' This is usually `TRUE`.
+#'
+#' @param creds `gitcreds`
+#' @return `TRUE` is there is a `password`
+#'
+#' @noRd
+
 has_password <- function(creds) {
   is_string(creds$password) && creds$password != ""
 }
+
+#' Create a string that can be passed as standard input to `git credential`
+#' commands
+#'
+#' @param args Usually a `gitcreds` object, but can be a named list in
+#' general. This is a format: https://git-scm.com/docs/git-credential#IOFMT
+#' @return String.
+#'
+#' @noRd
 
 create_gitcreds_input <- function(args) {
   paste0(
@@ -276,6 +396,9 @@ create_gitcreds_input <- function(args) {
     "\n\n"
   )
 }
+
+#' Environment to set for all `git credential` commands.
+#' @noRd
 
 gitcreds_env <- function() {
   # Avoid interactivity and validation with some common credential helpers
@@ -285,6 +408,13 @@ gitcreds_env <- function() {
     GCM_VALIDATE = "false"
   )
 }
+
+#' Check if `git` is installed and can run
+#'
+#' If not installed, a `gitcreds_nogit_error` is thrown.
+#'
+#' @return Nothing
+#' @noRd
 
 check_for_git <- function() {
   # This is simpler than Sys.which(), and also less fragile
@@ -299,6 +429,19 @@ check_for_git <- function() {
   if (!has_git) throw(new_error("gitcreds_nogit_error"))
 }
 
+#' Parse standard output from `git credential fill`
+#'
+#' @details
+#' For dummy credentials (i.e. the lack of credentials), it throws an
+#' error of class `gitcreds_no_credentials`.
+#'
+#' @param txt Character vector, standard output lines from
+#' `git credential fill`.
+#' @param url URL we queried, to be able to create a better error message.
+#' @return `gitcreds` object.
+#'
+#' @noRd
+
 parse_gitcreds_output <- function(txt, url) {
   if (is.null(txt) || txt[1] == "protocol=dummy") {
     throw(new_error("gitcreds_no_credentials", url = url))
@@ -307,6 +450,21 @@ parse_gitcreds_output <- function(txt, url) {
   vls <- sub("^[^=]+=", "", txt)
   structure(as.list(vls), names = nms, class = "gitcreds")
 }
+
+#' Query the `username` to use for `git config credential`
+#'
+#' @details
+#' The current working directory matters for this command, as you can
+#' configure `username` in a local `.git/config` file (via
+#' `git config --local`).
+#'
+#' @param url URL to query the username for, or `NULL`. If not `NULL`,
+#' then we first try to query an URL-specific username. See
+#' https://git-scm.com/docs/gitcredentials for more about URL-specific
+#' credential config
+#' @return A string with the username, or `NULL` if no default was found.
+#'
+#' @noRd
 
 gitcreds_username <- function(url = NULL) {
   gitcreds_username_for_url(url) %||% gitcreds_username_generic()
@@ -332,6 +490,12 @@ gitcreds_username_generic <- function() {
     }
   )
 }
+
+#' User name to use when creating a credential, if there is nothing better
+#'
+#' These user names are typical for some git tools.
+#'
+#' @noRd
 
 default_username <- function() {
   if (.Platform$OS.type == "windows") "PersonalAccessToken" else "token"
@@ -368,6 +532,15 @@ throw <- function(cond) {
 # Genetic helpers
 # ------------------------------------------------------------------------
 
+#' Set/remove env var and return the old values
+#'
+#' @param envs Named character vector or list of env vars to set. `NA`
+#' values will un-set an env var.
+#' @return Character vector, the old values of the supplied environment
+#' variables, `NA` for the ones that were not set.
+#'
+#' @noRd
+
 set_env <- function(envs) {
   current <- Sys.getenv(names(envs), NA_character_)
   na <- is.na(envs)
@@ -380,10 +553,28 @@ set_env <- function(envs) {
   invisible(current)
 }
 
+#' Get the user name from a `protocol://username@host/path` URL.
+#'
+#' @param url URL
+#' @return String or `NULL` if `url` does not have a username.
+#'
+#' @noRd
+
 get_url_username <- function(url) {
   nm <- parse_url(url)$username
   if (nm == "") NULL else nm
 }
+
+#' Parse URL
+#'
+#' It does not parse query parameters, as we don't deal with them here.
+#' The port number is included in the host name, if present.
+#'
+#' @param url Character vector of one or more URLs.
+#' @return Data frame with string columns: `protocol`, `username`,
+#' `password`, `host`, `path`.
+#'
+#' @noRd
 
 parse_url <- function(url) {
   re_url <- paste0(
@@ -404,6 +595,8 @@ is_string <- function(x) {
 has_no_newline <- function(url) {
   ! grepl("\n", url, fixed = TRUE)
 }
+
+# From the rematch2 package
 
 re_match <- function(text, pattern, perl = TRUE, ...) {
 
@@ -448,6 +641,16 @@ null_file <- function() {
 
 `%||%` <- function(l, r) if (is.null(l)) r else l
 
+#' Like [message()], but print to standard output in interactive
+#' sessions
+#'
+#' To avoid red output in RStudio, RGui, and R.app.
+#'
+#' @inheritParams message
+#' @return Nothing
+#'
+#' @noRd
+
 msg <- function(..., domain = NULL, appendLF = TRUE) {
   cnd <- .makeMessage(..., domain = domain, appendLF = appendLF)
   withRestarts(muffleMessage = function() NULL, {
@@ -455,15 +658,32 @@ msg <- function(..., domain = NULL, appendLF = TRUE) {
     output <- default_output()
     cat(cnd, file = output, sep = "")
   })
+  invisible()
 }
+
+#' Where to print messages to
+#'
+#' If the session is not interactive, then it potentially matters
+#' whether we print to stdout or stderr, so we print to stderr.
+#'
+#' The same applies when there is a sink for stdout or stderr.
+#'
+#' @return The connection to print to.
+#'
+#' @noRd
 
 default_output <- function() {
   if (is_interactive() && no_active_sink()) stdout() else stderr()
 }
 
 no_active_sink <- function() {
+  # See ?sink.number for the explanation
   sink.number("output") == 0 && sink.number("message") == 2
 }
+
+#' Smarter `interactive()`
+#'
+#' @noRd
 
 is_interactive <- function() {
   opt <- getOption("rlib_interactive")
@@ -487,9 +707,23 @@ is_interactive <- function() {
   }
 }
 
+#' Squote wrapper to avoid smart quotes
+#'
+#' @inheritParams sQuote
+#' @inherit sQuote return
+#' @noRd
+
 squote <- function(x) {
   sQuote(x, FALSE)
 }
+
+#' Read all of a file
+#'
+#' @param path File to read.
+#' @param ... Passed to [readChar()].
+#' @return String.
+#'
+#' @noRd
 
 read_file <- function(path, ...) {
   readChar(path, nchars = file.info(path)$size, ...)

a51afd68592a571e780ae820e3f4fea82478ce26
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-09-24 12:36:13 -0700

    Use in-house technology to set/reset env var

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,10 +1,9 @@
 test_that("gitcreds handles env var set to empty string", {
   skip_on_os("windows")
 
-  orig <- Sys.getenv("GITHUB_PAT_GITHUB_ACME_COM")
-  on.exit(Sys.setenv(GITHUB_PAT_GITHUB_ACME_COM = orig), add = TRUE)
+  oenv <- set_env(c(GITHUB_PAT_GITHUB_ACME_COM = ""))
+  on.exit(set_env(oenv), add = TRUE)
 
-  Sys.setenv(GITHUB_PAT_GITHUB_ACME_COM = "")
   x <- gitcreds::gitcreds_get("https://github.acme.com")
   expect_equal(x$password, "")
 })

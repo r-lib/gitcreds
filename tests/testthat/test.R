ffdd0933c392b44967e7f98522db3888a03c8af2
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-09-24 12:31:56 -0700

    Skip test on windows

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,4 +1,6 @@
 test_that("gitcreds handles env var set to empty string", {
+  skip_on_os("windows")
+
   orig <- Sys.getenv("GITHUB_PAT_GITHUB_ACME_COM")
   on.exit(Sys.setenv(GITHUB_PAT_GITHUB_ACME_COM = orig), add = TRUE)
 

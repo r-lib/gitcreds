89918900e884af645a195dca80bb704a7a233212
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-09-25 10:10:30 -0700

    Add failing test

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -7,3 +7,13 @@ test_that("set_env() round trip works with just 1 env var", {
   set_env(oenv)
   expect_equal(Sys.getenv("FOO", "unset"), "unset")
 })
+
+test_that("env var set to empty string is same as unset", {
+  skip_on_os("windows")
+
+  oenv <- set_env(c(GITHUB_PAT_GITHUB_ACME_COM = ""))
+  on.exit(set_env(oenv), add = TRUE)
+
+  x <- gitcreds_get_cache("GITHUB_PAT_GITHUB_ACME_COM")
+  expect_null(x$password)
+})

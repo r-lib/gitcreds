67ca3bbb74504baebec03dacee0b1d3b45c6543a
Gábor Csárdi <csardi.gabor@gmail.com>
2025-04-29 14:38:19 +0200

    Only skip if _R_CHECK_FORCE_SUGGESTS_ is false

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -144,3 +144,21 @@ local_helpers <- function(helpers, .local_envir = parent.frame()) {
     gitcreds$git_run(c("config", "--global", "--add", "credential.helper", helper))
   }
 }
+
+isFALSE <- function(x) {
+  is.logical(x) && length(x) == 1L && !is.na(x) && !x
+}
+
+is_false_check_env_var <- function(x, default = "") {
+  # like utils:::str2logical
+  val <- Sys.getenv(x, default)
+  if (isFALSE(as.logical(val))) return(TRUE)
+  tolower(val) %in% c("0", "no")
+}
+
+# Only skip if _R_CHECK_FORCE_SUGGESTS_ is false
+
+skip_if_not_installed <- function(pkg) {
+  if (!is_false_check_env_var("_R_CHECK_FORCE_SUGGESTS_")) return()
+  testthat::skip_if_not_installed(pkg)
+}
